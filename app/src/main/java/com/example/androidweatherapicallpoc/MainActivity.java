6be55f4145022b39ba60ec26ac5eeecf9f22ee49
Pawel Rudnicki <p.j.rudnicki@gmail.com>
2019-02-01 13:48:32 +0000

    Added webservices, Created API_KEY on weather website

--- app/src/main/java/com/example/androidweatherapicallpoc/MainActivity.java
+++ app/src/main/java/com/example/androidweatherapicallpoc/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.androidweatherapicallpoc;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // go back to default theme after splash screen
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
+
+        String city = "London,UK";
+
+        JSONWeatherTask task = new JSONWeatherTask();
+        task.execute(new String[]{city});
+    }
+
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+            Weather weather = new Weather();
+            String data = ((new WeatherHttpClient()).getWeatherData(params[0]));
+
+            try {
+                weather = JSONWeatherParser.getWeather(data);
+
+                // Let's retrieve the icon
+                weather.iconData = ((new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return weather;
+
+        }
+
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+
+            if (weather.iconData != null && weather.iconData.length > 0) {
+                Bitmap img = BitmapFactory.decodeByteArray(weather.iconData, 0, weather.iconData.length);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/androidweatherapicallpoc/WeatherHttpClient.java
@@ -0,0 +1,84 @@
+package com.example.androidweatherapicallpoc;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WeatherHttpClient {
+
+    //http://api.openweathermap.org/data/2.5/weather?q=city,country&APPID={your_key}
+    private final static String API_KEY = "4beacd07fa3127c62b1c45e05c6fb3d9";
+    private final static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private static String IMG_URL = "http://openweathermap.org/img/w/";
+
+    public String getWeatherData(String location) {
+        HttpURLConnection connection = null ;
+        InputStream inputStream = null;
+
+        try {
+            String address = BASE_URL + location + "&APPID=" + API_KEY + "";
+            connection = (HttpURLConnection) (new URL(address).openConnection());
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.connect();
+
+            // Let's read the response
+            StringBuffer buffer = new StringBuffer();
+            inputStream = connection.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while (  (line = br.readLine()) != null )
+                buffer.append(line + "\r\n");
+
+            inputStream.close();
+            connection.disconnect();
+            return buffer.toString();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        finally {
+            try { inputStream.close(); } catch(Throwable t) {}
+            try { connection.disconnect(); } catch(Throwable t) {}
+        }
+
+        return null;
+
+    }
+
+    public byte[] getImage(String code) {
+        HttpURLConnection con = null ;
+        InputStream is = null;
+        try {
+            con = (HttpURLConnection) ( new URL(IMG_URL + code)).openConnection();
+            con.setRequestMethod("GET");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.connect();
+
+            // Let's read the response
+            is = con.getInputStream();
+            byte[] buffer = new byte[1024];
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            while ( is.read(buffer) != -1)
+                baos.write(buffer);
+
+            return baos.toByteArray();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        finally {
+            try { is.close(); } catch(Throwable t) {}
+            try { con.disconnect(); } catch(Throwable t) {}
+        }
+
+        return null;
+
+    }
+}
